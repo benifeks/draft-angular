894831585ac7a5652bde0b3a1b1852ef0edcc95c
Sergey <benifeks72@gmail.com>
2025-07-09 22:27:09 +0300

    Created contact form component with emailjs integration and footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFacebook, faTelegram } from '@fortawesome/free-brands-svg-icons';
+
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
+  public icons = {
+    telegram: faTelegram,
+    facebook: faFacebook,
+  };
 
+  public year: number = new Date().getFullYear();
 }
--- src/app/pages/contact-mail/contact-mail.component.ts
+++ src/app/pages/contact-mail/contact-mail.component.ts
@@ -1,9 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import emailjs from 'emailjs-com';
 
 @Component({
   selector: 'app-contact-mail',
-  imports: [],
   templateUrl: './contact-mail.component.html',
   styleUrl: './contact-mail.component.scss',
+  imports: [CommonModule, FormsModule],
 })
-export class ContactMailComponent {}
+export class ContactMailComponent {
+  formData = {
+    user_email: '',
+    message: '',
+  };
+
+  status: string = '';
+
+  handleSubmit(event: Event): void {
+    event.preventDefault();
+    this.status = 'Отправка...';
+
+    emailjs
+      .send(
+        'service_pt0oeps',
+        'template_aet7agt',
+        this.formData,
+        'eQ-bleyioVcVMvpt2'
+      )
+      .then(
+        () => {
+          this.status = 'Сообщение отправлено успешно!';
+          this.formData = { user_email: '', message: '' };
+        },
+        () => {
+          this.status = 'Не удалось отправить сообщение, попробуйте ещё раз.';
+        }
+      );
+  }
+}
