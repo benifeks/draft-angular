88def01c0459aa0fe4839458685dc5df3ade6c5a
Sergey <benifeks72@gmail.com>
2025-07-10 14:52:48 +0300

    About component adapted from React to Angular 19

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 
+import { ABOUT_CARDS } from './about-cards';
+
 @Component({
   selector: 'app-about',
   imports: [],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
 })
 export class AboutComponent {
-
+  public aboutCards = ABOUT_CARDS;
+  public spritePath = 'assets/images/about/symbol-defs.svg';
 }
