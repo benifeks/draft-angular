a933c41c854066855ef61af6fcbe02ef2e8b53e5
Sergey <benifeks72@gmail.com>
2025-07-10 11:41:02 +0300

    Адаптирован React-компонент MyCourses под Angular 19

--- src/app/pages/my-courses/my-courses.component.ts
+++ src/app/pages/my-courses/my-courses.component.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { COURSES } from './courses';
 
 @Component({
   selector: 'app-my-courses',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './my-courses.component.html',
-  styleUrl: './my-courses.component.scss'
+  styleUrl: './my-courses.component.scss',
 })
 export class MyCoursesComponent {
-
+  public courses = COURSES;
+  public spritePath = 'assets/images/svg/technologies/symbol-defs.svg';
 }
